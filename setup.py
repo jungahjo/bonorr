05a2f354c47f7989a3f8dbecdcdce4a5d146589e
jungahjo <134163658+jungahjo@users.noreply.github.com>
2024-02-22 13:27:25 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 setup(
-    name                = 'ypcc22',
+    name                = 'bonorr',
     version             = '0.7.1',
     long_description    = long_description,
     long_description_content_type='text/markdown',
@@ -11,13 +11,13 @@ setup(
     license             = 'MIT',
     author              = 'bono',
     author_email        = 'ellen333@naver.com',
-    url                 = 'https://github.com/jungahjo/ypcc22',
+    url                 = 'https://github.com/jungahjo/bonorr',
     download_url        = 'https://github.com/jungahjo/ypcc22/archive/0.0.tar.gz',
     install_requires    = [
         "numpy < 1.15"
     ],
     packages            = find_packages(exclude = []),
-    keywords            = ['ypcc'],
+    keywords            = ['bonorr'],
     python_requires     = '>=3',
     package_data        = {},
     zip_safe            = False,
